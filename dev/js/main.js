b736e8b5df8ee5bee5b8008ae07f73f1ea06dece
Viktor <viktor@creus.se>
2015-12-06 16:38:59 +0100

    Flyttade utvecklingen till /dev. Tillfällig landningssida i rotmappen

---
+++ dev/js/main.js
@@ -0,0 +1,37 @@
+$( document ).ready(function() {
+  'use strict';
+
+  /* Ange att innehållet har laddats */
+  $( "body" ).addClass( "loaded" );
+});
+
+$(function(){
+  /* Mjuk laddning av sidor */
+  var options = {
+    debug: true,
+    prefetch: true,
+    prefetchOn: 'mouseover touchstart',
+    cacheLength: 4,
+    onStart: {
+      duration: 1000, // Duration of our animation
+      render: function ($container) {
+        // Add your CSS animation reversing class
+        $container.addClass( 'is-exiting' );
+
+        // Restart your animation
+        smoothState.restartCSSAnimations();
+      }
+    },
+    onReady: {
+      duration: 0,
+      render: function ($container, $newContent) {
+        // Remove your CSS animation reversing class
+        $container.removeClass('is-exiting');
+
+        // Inject the new content
+        $container.html($newContent);
+      }
+    }
+  },
+  smoothState = $('#main').smoothState(options).data('smoothState');
+});
